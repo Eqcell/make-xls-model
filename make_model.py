0e5e721a191c0cc6fa8229c44fdac04e61d9382b
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-17 13:26:31 +0300

    Update make_model.py

--- make_model.py
+++ make_model.py
@@ -1,4 +1,4 @@
-# coding: cp1251
+# coding: utf-8
 """
    Generate Excel file with ordered rows containing Excel formulas 
    that allow to calculate forecast values based on historic data, 
@@ -10,47 +10,96 @@
         equations
         names
         controls (forecast parameters)
-        format
-        start_year        
+        row_labels
+        years
+        
    Output: 
         macro.xls
+        (now - an array of values to be written to macro.xls)
 """
 
 import numpy as np
-
         
+from data_source import get_historic_data_as_dataframe, get_names_as_dict
+from data_source import get_equations
+from data_source import get_controls_as_dataframe
+from data_source import get_row_labels, get_years_as_list, get_xl_filename
 
-# todo: interface to eqcell.py
-        
-#***********************************************        
+    
+# when set to seros, the cellblock on output file will start at A1
+CELLBLOCK_OFFSET_BY_ROW = 0 #5 
+CELLBLOCK_OFFSET_BY_COL = 0 #2
 
-YEAR_ROW = 2 
-LABEL_COLUMN = 3
-CELL_TYPES = {'value':0, 'formula':1}
-
-def
-
-
-def create_empty_array(format):   
-   max_rows = len(format['rows']) + 1
-   max_col = 2 +(get_max_control_year() - format['start_year'] + 1)    
+def create_empty_cellblock_array(row_labels, years):   
+   max_rows = 1 + len(row_labels) + CELLBLOCK_OFFSET_BY_ROW   
+   max_col  = 2 + len(years) + CELLBLOCK_OFFSET_BY_COL 
    return np.empty((max_rows, max_col), dtype = object)
 
-range_array = create_empty_array(format)
+def populate_cellblock_with_labels_and_titles(cellblock, row_labels, names):   
+    for j, label in enumerate(row_labels):
+        # writing (label, names[label]['title']) to first two columns
+        # better - write to temporary cellblock, not offsetted, 
+        #          but make actual(pffsetted) cellblock before populate_cellblock_with_equations()        
+        #          where actual offsets will be needed
+        cellblock[1 + CELLBLOCK_OFFSET_BY_ROW + j, CELLBLOCK_OFFSET_BY_COL]     = names[label]['title']
+        cellblock[1 + CELLBLOCK_OFFSET_BY_ROW + j, CELLBLOCK_OFFSET_BY_COL + 1] = label
+    return cellblock
+        
+def populate_cellblock_with_years(cellblock, years):
+    cellblock[0 + CELLBLOCK_OFFSET_BY_ROW,:] = [""] * CELLBLOCK_OFFSET_BY_COL + ["",""] + years
+    # when not offsetted:
+    # cellblock[0,:] = ["",""] + years
+    return cellblock
+    
+def populate_cellblock_with_data(cellblock, row_labels, data):    
+    # todo
+    return cellblock
+    
+def populate_cellblock_with_controls(cellblock, row_labels, row_titles):
+    # todo
+    return cellblock
+    
+def populate_cellblock_with_equations(cellblock, row_labels, equations):
+    return cellblock   
+        
+def dump_cellblock_to_xls(xl_filename, cellblock):
+    print("\nThis will dump cellblock to " + xl_filename)  
+    print(cellblock)
 
-# row = YEAR_ROW - 1,
-# col = LABEL_COLUMN - 1
- 
- 
-   
-for j, label in enumerate(format['rows']):
-    range_array[1 + j, 1] = label     #  (label, CELL_TYPES['value'])
-    range_array[1 + j, 0] = 'Label text here' # label #  ('Label text here', CELL_TYPES['value'])  
-print (range_array) 
-
-#***********************************************    
-
-
-
-
-
+if __name__ == "__main__":      
+    #init external variables
+    data = get_historic_data_as_dataframe()  
+    names = get_names_as_dict()
+    equations = get_equations()
+    controls = get_controls_as_dataframe()
+    row_labels = get_row_labels()
+    years = get_years_as_list()
+      
+    cellblock = create_empty_cellblock_array(row_labels, years)    
+    cellblock = populate_cellblock_with_labels_and_titles(cellblock, row_labels, names)    
+    cellblock = populate_cellblock_with_years(cellblock, years)
+    
+    #todo: make two functions below insert actual data 
+    #      note - must use *years* and *row_labels* as pivot    
+    cellblock = populate_cellblock_with_data(cellblock, row_labels, data)
+    cellblock = populate_cellblock_with_controls(cellblock, row_labels, controls)   
+    
+    # --- Current output is: 
+    # This will dump cellblock to macro.xls
+    # [['' '' 2013 2014 2015 2016]
+    # ['ВВП' 'GDP' None None None None]
+    # ['Дефлятор ВВП' 'GDP_IP' None None None None]
+    # ['Индекс физ.объема ВВП' 'GDP_IQ' None None None None]]
+    
+    # after changes are implemented only two None's will be there
+    # ### indicates a digit
+    # [['' '' 2013 2014 2015 2016]
+    # ['ВВП' 'GDP' ### ### None None]
+    # ['Дефлятор ВВП' 'GDP_IP' ### ### ### ###]
+    # ['Индекс физ.объема ВВП' 'GDP_IQ' ### ### ### ###]]
+    
+    # not todo:
+    cellblock = populate_cellblock_with_equations(cellblock, row_labels, equations)    
+    
+    xl_filename = get_xl_filename()
+    dump_cellblock_to_xls(xl_filename, cellblock)
