b2130f556a76607e6195275e28cb0f029aaffa9c
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-17 13:38:33 +0300

    Update data_source.py

--- data_source.py
+++ data_source.py
@@ -1,34 +1,38 @@
 
-# ****************************************** 
-# Proxies to speed up development / testing
-# *****************************************
+# ***************************************************************************** 
+# Proxies of data import form xls/csv/mysql (to speed up development / testing)
+# *****************************************************************************
 
 import io
 import csv
 from pprint import pprint
+import pandas as pd
 
-
-def get_historic_data_as_dataframe():
-    return None
-
-       
-def get_data(label, year):
-    data_proxy = [ ("GDP", 2013, 66190.11992)
+data_proxy = [ ("GDP", 2013, 66190.11992)
         , ("GDP",    2014, 71406.3992)
         , ("GDP_IQ", 2013, 101.3407976)
         , ("GDP_IQ", 2014, 100.6404858)
         , ("GDP_IP", 2013, 105.0467483)
         , ("GDP_IP", 2014, 107.1941886)
-        ]
+        ] 
+        
+def get_historic_data_as_dataframe():
+    #todo: must return a dataframe based on *data_proxy*
+    z = {     "GDP" : [66190.11992, 71406.3992 ]
+          , "GDP_IQ": [101.3407976, 100.6404858]       
+          , "GDP_IP": [105.0467483, 107.1941886]} 
+    return pd.DataFrame(z, index = [2013, 2014])
+        
+def get_data(label, year, data_proxy):
     slice = [x for x in data_proxy if x[0] == label]
     return  [x for x in slice if x[1] == year][0]
 
 
 def yield_names_proxy():
     #names_csv_proxy ="""title	label	group	level	precision
-    names_csv_proxy ="""ВВП	gdp	Нацсчета	1	0
-Индекс физ.объема ВВП	gdp_Iq	Нацсчета	2	1
-Дефлятор ВВП	gdp_Ip	Нацсчета	2	1"""    
+    names_csv_proxy ="""ВВП	GDP	Нацсчета	1	0
+Индекс физ.объема ВВП	GDP_IQ	Нацсчета	2	1
+Дефлятор ВВП	GDP_IP	Нацсчета	2	1"""    
     text_stream = io.StringIO(names_csv_proxy)
     # better use csv.DictReader and work with dict in get_names_as_dict()
     reader = csv.reader(text_stream, delimiter='\t')
@@ -47,15 +51,21 @@ def get_names_as_dict():
         names_dict.update(new_entry_dict)
     return names_dict
 
-def get_controls_as_dataframe():
-    return None
-   
-controls_proxy = [("GDP_IP", 2015, 95,0)
-        , ("GDP_IQ", 2015, 115,0)
+controls_proxy = [("GDP_IP", 2015, 95.0)
+        , ("GDP_IQ", 2015, 115.0)
+        , ("GDP_IP", 2016, 102.5)
+        , ("GDP_IQ", 2016, 113.0)
         ]
+        
+def get_controls_as_dataframe():
+    #todo: must return a dataframe based on *controls_proxy*
+    z = {'GDP_IP' : [95.0, 102.5],
+         'GDP_IQ' : [115.0, 113.0]}   
+    return pd.DataFrame(z, index=[2015, 2016])
+
 
 def get_years_as_list():
-    return range(get_start_year(),get_max_control_year() + 1)
+    return [y for y in range(get_start_year(),get_max_control_year() + 1)]
 
 def get_max_control_year():
     return max([x[1] for x in controls_proxy])
@@ -67,12 +77,12 @@ def get_equations():
 def get_sheet_format():
     return { 'filename':'macro.xls' 
         , 'sheet': 'gdp_forecast'
-        , 'start_year': 2014
+        , 'start_year': 2013
         , 'rows': ["GDP", "GDP_IP", "GDP_IQ"]
         }
         
 def get_xl_filename():
-    return get_sheet_format()['macro.xls']
+    return get_sheet_format()['filename']
       
 def get_sheet_name():
     return get_sheet_format()['sheet']
@@ -83,33 +93,41 @@ def get_start_year():
 def get_row_labels():        
     return get_sheet_format()['rows']
 
-  
-  
+    
 final_dataframe_proxy = """		2014	2015	2016
 ВВП	gdp	71406	=D3*E4/100*E5/100	=E3*F4/100*F5/100
 Индекс физ.объема ВВП	gdp_Iq	100,6	95	102
 Дефлятор ВВП	gdp_Ip	107,2	110	112
 """        
 
+# from data_source import get_historic_data_as_dataframe, get_names_as_dict
+# from data_source import get_controls_as_dataframe
+# from data_source import get_row_labels, get_years_as_list
+
 data = get_historic_data_as_dataframe()  
-print("------ Data:")
-pprint(data) 
-
 names = get_names_as_dict()
-print("------ Names:")
-pprint(names)  
-
+equations = get_equations()
 controls = get_controls_as_dataframe()
-print("------ Control forecast parameters:")
-pprint(controls)  
-
 row_labels = get_row_labels()
-print("------ Row labels:")
-pprint(row_labels)  
-
 years = get_years_as_list()
-print("------ Years:")
-pprint(years)  
 
-pprint(final_dataframe_proxy)
+if __name__ == "__main__":
+    print("------ Data:")
+    pprint(data) 
 
+    print("------ Names:")
+    pprint(names)  
+
+    print("------ Control forecast parameters:")
+    pprint(controls)  
+
+    print("------ Row labels:")
+    pprint(row_labels)  
+
+    print("------ Years:")
+    pprint(years) 
+
+    print("------ Equations:")
+    pprint(equations)
+
+    pprint(final_dataframe_proxy)
