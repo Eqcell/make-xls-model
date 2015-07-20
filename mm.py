800388db7c480a1218fe782db8f8229bd0df8ff0
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-20 10:50:26 +0300

    Update mm.py

--- mm.py
+++ mm.py
@@ -21,18 +21,18 @@ from yaml_parser import get_user_param, get_default_param
 # make model and output specification based on user input
 # model_dict, view_dict = get_specification(model_user_param_dict, view_user_param_dict)
 # get defaults 
-model_dict, view_dict = get_sample_specifcation(None, None)
+model_spec, view_spec = get_sample_specification()
 
 
 ####### 
 ####### 3. Do main job
 #######  
 # create an numpy array, representing resulting worksheet
-wb_array = make_wb_array(model_dict, view_dict)
+wb_array = make_wb_array(model_spec, view_spec)
 
 
 ####### 
-####### Write results to file 
+####### 4. Write results to file 
 #######  
 # write array to output excel file 
 # note: upon implementation can be a new sheet in same file (e.g. xlwings, even for an open file)
@@ -42,4 +42,15 @@ write_output_to_xls(wb_array, view_dict)
 #     if no user specification - write all controls in orginal order,
 #                              - followed by data without controls in orginal order
 #                              - year depth as in historic data
+#
+
+#
+# 2015-07-20 10:46 AM
+#
+# Outline:
+# - write to xls
+# - one input xls file
+# - larger xls file (flatten folder)
+# - experiment with actual data - full cycle (flatten folder)
+# - (source + markup) vs flattened db vs db dump
 #
