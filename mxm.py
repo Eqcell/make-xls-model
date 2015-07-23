980656e005029adf4bd29edc2322fbed27492d3d
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-23 17:03:30 +0300

    Update mxm.py

--- mxm.py
+++ mxm.py
@@ -2,44 +2,29 @@
    control variables and spreadsheet parameters.      
    
 Usage:   
-   mxm.py --selftest
-   mxm.py <SPEC_FILE> 
-   mxm.py --markup <YAML_FILE>
+   mxm.py --test
+   mxm.py <XLFILE> [--markup <YAML_FILE>]
 """
 
 from docopt import docopt
-from read_xls import get_specification_from_arg
-from xl_fill import make_wb_array
-from xls_io import write_output_to_xls
+import os
+from make_xl_model import make_xl_model
 
-from xlwings import Workbook, Range, Sheet
-import numpy as np
+def get_abs_filepath(arg):
+    """Returns absolute path to <XLFILE>"""
+    return os.path.abspath(arg["<XLFILE>"])
+    
+def get_model_sheet(arg):
+    MODEL_SHEET = 'model'
+    return MODEL_SHEET
 
-
-def write_output_to_xls(wb_array, view_spec)
-    file = view_spec['file']
-    sheet = view_spec['model']
-    wb = Workbook(file)
-    Range(sheet, 'A1').value = ar
-    Sheet(sheet).activate()
-    wb.save()
-    # do not close file
-    # wb.close()
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     arg = docopt(__doc__)
-
-    # init model parameters
-    # model_spec, view_spec = get_specification_from_arg(arg)
-
-    # main job of creating resulting workbook array 'wb_array', dumpable to Excel
-    # wb_array = make_wb_array(model_spec, view_spec)
-
-    from data_source import _sample_for_xfill_array_after_equations 
-    ar =  _sample_for_xfill_array_after_equations()
-
-    view_spec = {'file': 'D:/make-xls-model-master/spec.xls', 
-                'sheet': 'model'}
-
-    # dump 'wb_array' to Excel
-    write_output_to_xls(wb_array, view_spec)
+    
+    file = get_abs_filepath(arg)
+    sheet = get_model_sheet(arg)    
+    
+    make_xl_model(file, sheet)
+    
+    
+    
