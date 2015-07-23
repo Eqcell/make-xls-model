05a7b66530e7b0324bc34c18a4efbf7c465c2fb8
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-23 17:11:22 +0300

    Update xl_fill.py

--- xl_fill.py
+++ xl_fill.py
@@ -83,10 +83,6 @@ def check_get_dataframe_before_equations():
     [data_df, names_dict, equations_list, controls_df] = [s[1] for s in model_spec]      
     df1 = get_sample_df_before_eq()
     df2 = get_dataframe_before_equations(data_df, controls_df, var_label_list)  
-    # print("Sample")    
-    # print(df1)
-    # print("Own calculation")    
-    # print(df2)
     return df1.equals(df2)
 
 def get_dataframe_before_equations(data_df = None, controls_df = None, var_label_list = None):    
@@ -132,104 +128,57 @@ def get_array_before_equations(df):
     ar = df.as_matrix().transpose().astype(object)
     labels = df.columns.tolist() 
     ar = np.insert(ar, 0, labels, axis = 1)
-    years = [""] + df.index.tolist()
+    years = [""] + df.index.astype(str).tolist()
     ar = np.insert(ar, 0, years, axis = 0)
     return ar
-
-###########################################################################
-## Parsing of formulas (LATER - can move to eqcell_core.py)
-###########################################################################
-    
-def strip_timeindex(str_):
-    """Returns variable name without time index.
-    TODO: if function cannot strip time index anything return None
-
-    Tests:
-    >>> strip_timeindex("GDP(t)")
-    'GDP'
-
-    WARNING: in no time index return 'str_'. Test below fails.
-    #>>> strip_timeindex("GDP")
-    #'GDP'
-    """
-    all_indices = "".join(TIME_INDEX_VARIABLES)
-    pattern = r"(\S*)[\[(][" + all_indices + "][)\]]"
-    m = re.search(pattern, str_)
-    if m:
-        return m.groups()[0]
-    else:
-        return None
-        
-def test_parse_to_formula_dict():    
-    """
-    >>> test_parse_to_formula_dict()
-    True
-    True
-    True
-    """
-    inputs = [
-      ['GDP(t) = GDP(t-1) * GDP_IP(t) / 100 * GDP_IQ(t) / 100']
-    , ['x(t) = x(t-1) + 1']
-    , ['x(t) = x(t-1) + 1', 'y(t) = x(t)']
-    ]    
-    expected_outputs = [
-      {'GDP': {'dependent_var': 'GDP(t)', 'formula': 'GDP(t-1) * GDP_IP(t) / 100 * GDP_IQ(t) / 100'}}   
-    , {'x': {'dependent_var': 'x(t)', 'formula': 'x(t-1) + 1'}}
-    , {'x': {'dependent_var': 'x(t)', 'formula': 'x(t-1) + 1'}, 'y': {'dependent_var': 'y(t)', 'formula': 'x(t)'}}
-    ]
-    for input_eq, expected_output in zip(inputs,expected_outputs):
-       print(expected_output == parse_to_formula_dict(input_eq))
-
-def parse_to_formula_dict(equations):
-    """Returns a dict with left and right hand side of equations, referenced by variable name in keys."""
-    parsed_eq_dict = {}
-    for eq in equations:
-        dependent_var, formula = eq.split('=')
-        key = strip_timeindex(dependent_var)
-        parsed_eq_dict[key] = {'dependent_var': dependent_var.strip(), 'formula': formula.strip()}
-    return parsed_eq_dict
-
-
 
 ###########################################################################
 ## get_xl_formula - interface to eqcell_core.py and new text parser
 ###########################################################################
 
+def build_formula(var_name, equations_list):
+    """Returns a string like 'x(t-1) + 1' as a formula.""" 
+    # DONE:          I consider splitting 'equations_list' into 'formulas_dict' a common task 
+    #                for both parsers, but in concept this does not really belong to 'xl_fill.py'
+    #                
+    #                May later move strip_timeindex(str_), test_parse_to_formula_dict() and 
+    #                parse_to_formula_dict(equations) to a separate module, callable either by two parsers OR
+    #                at stage where we evaluate user-defined input. 
+    import equations_preparser as eq_pp
+    formulas_dict     = eq_pp.parse_to_formula_dict(equations_list)
+    return eq_pp.get_formula(var_name, formulas_dict)
+
+
+
 def get_xl_formula(cell, var_name, equations_list, variables_dict):
-        # COMMENT - EP:  I consider splitting 'equations_list' into 'formulas_dict' a common task 
-        #                for both parsers, but in concept this does not really belong to 'xl_fill.py'
-        #                May later move strip_timeindex(str_), test_parse_to_formula_dict() and 
-        #                parse_to_formula_dict(equations) to a separate module, callable either by two parsers OR
-        #                at stage where we evaluate user-defined input.
-        # Additionally:  - we currently do not check formulas: 
-        #                   * Only current period vars allowed on left side. Valid: "x(t)= x(t-1)". Not Valid: "x(t+1) = x(t)"
-        #                   * All required varables must be covered by formulas
-        #                   * What happens if there is a variable that is created from other variables, but not listed in data or controls?
-        #                - perhaps it is enough to pass only left-habd side of 'equation_as_dict' further. 
-        #                  EP:Done    
-        #                - cross-dependencies of 'TIME_INDEX_VARIABLES'       
-        
-        formulas_dict = parse_to_formula_dict(equations_list)
-        equation_as_dict = formulas_dict[var_name]
-        formula_as_string = equation_as_dict['formula']
-        return get_xl_formula_sympyparser(cell, var_name, formula_as_string, variables_dict)
+    """Returns a valid Excel formula as string, eg '=A2*C3'.   """ 
+    # LATER:  - we currently do not check formulas: 
+    #            * Only current period vars ьгые иу allowed on left side. Valid: "x(t)= x(t-1)". Not Valid: "x(t+1) = x(t)"
+    #            * All required varables must be covered by formulas
+    #            * What happens if there is a variable that is created from other variables, but not listed in data or controls?
+    #          - cross-dependencies of 'TIME_INDEX_VARIABLES'       
+    
+    # get formula to work with
+    formula_as_string =  build_formula(var_name, equations_list)
+    # use sympy parser
+    return get_xl_formula_sympyparser(cell, var_name, formula_as_string, variables_dict)
 
 def get_xl_formula_textparser(cell, var_name, formula_as_string, variables_dict):
-        # interface to new parsing algorithm 
-        return None
+    # interface to new parsing algorithm 
+    return None
         
 def get_xl_formula_sympyparser(cell, var_name, formula_as_string, variables_dict):
-        """
-        cell is (row, col) tuple
-        varname is like 'GDP'
-        formula_as_string is 'x(t-1) + 1'
-        variables_dict is like {'GDP': 2} - shows at which row the var is        
-        """         
-        try:
-            time_period = cell[1]            
-            return parse_equation_to_xl_formula(formula_as_string, variables_dict, time_period)
-        except KeyError:
-            return ""        
+    """
+    cell is (row, col) tuple
+    varname is like 'GDP'
+    formula_as_string is 'x(t-1) + 1'
+    variables_dict is like {'GDP': 2} - shows at which row the var is        
+    """         
+    try:
+        time_period = cell[1]            
+        return parse_equation_to_xl_formula(formula_as_string, variables_dict, time_period)
+    except KeyError:
+        return ""        
 
 ###########################################################################
 ## fill_array_with_excel_formulas
@@ -271,13 +220,14 @@ def get_variable_rows_as_dict(array, column = 0):
         return variable_to_row_dict
         # better - check is it is a valid variable name
         # var_list = unique(controls.columns.values.tolist() + row_labels)                
-        
+
 # def unique(list_):
     # """Returns unique elements from list.
     # >>> unique(['a','a'])
     # ['a']
     # """
-    # return list(set(list_))
+    # return list(set(list_))     
+
         
 def get_var_label(ar, row, var_column = 0):
         # WARNING: behavior not guaranteed, desired var_column may be not 0, must assign to constant
@@ -320,7 +270,9 @@ if __name__ == "__main__":
     print(ar)
     
     print("\n***  Split formulas:")
-    formulas_dict = parse_to_formula_dict(equations_list)
+    
+    import equations_preparser as eq_pp
+    formulas_dict = eq_pp.parse_to_formula_dict(equations_list)
     pprint(formulas_dict)
     
     print("\n***  Assign variables to array rows:")
