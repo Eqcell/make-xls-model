20f0be11370bd7d075c5cded08ebd7b0d68251ef
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-07-29 11:36:58 +0300

    update or make model

--- mxm.py
+++ mxm.py
@@ -2,28 +2,32 @@
    control variables and spreadsheet parameters.      
    
 Usage:   
-    mxm.py <xlfile> 
+    mxm.py <xlfile> [--make | --update]
 """
+
+MODEL_SHEET = 'model'
 
 from docopt import docopt
 import os
-from make_xl_model import make_xl_model
+from make_xl_model import make_xl_model, update_xl_model
 
 def get_abs_filepath(arg):
-    """Returns absolute path to <xlfile>"""
-    return os.path.abspath(arg["<xlfile"])
+   """Returns absolute path to <xlfile>"""
+   return os.path.abspath(arg["<xlfile"])
     
 def get_model_sheet(arg):
-    MODEL_SHEET = 'model'
-    return MODEL_SHEET
+   return MODEL_SHEET
 
 if __name__ == "__main__":
-    arg = docopt(__doc__)
+   arg = docopt(__doc__)
     
-    file = get_abs_filepath(arg)
-    sheet = get_model_sheet(arg)    
+   file = get_abs_filepath(arg)
+   sheet = get_model_sheet(arg)    
     
-    make_xl_model(file, sheet)
-    
-    
-    
+   if arg["--make"]:
+      make_xl_model(file, sheet)
+   elif arg["--update"]:
+      update_xl_model(file, sheet)
+   else:
+      make_xl_model(file, sheet)
+      
