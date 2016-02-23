bc045c35bbb2a7bc655bd9c30d3845944ee86d5a
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-02-24 01:49:17 +0300

    moved files around

---
+++ make_xls_model/examples/examples.bat
@@ -0,0 +1,6 @@
+python ..\mxm.py bdrn.xls -M
+python ..\mxm.py ref_file.xls -M
+python ..\mxm.py spec.xls -M
+python ..\mxm.py spec2.xls -M
+python ..\mxm.py bank.xls -M
+python ..\mxm.py bank_sector.xls -M
