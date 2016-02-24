ec62a4fdee902d5c4814ad935b611ad90cb0034d
=epogrebnyak <e.pogrebnyak@gmail.com>
2016-02-24 22:57:18 +0300

    py.test tests

--- make_xls_model/test_ref_file.py
+++ make_xls_model/test_ref_file.py
@@ -1,9 +1,11 @@
 import numpy as np 
 
+import os 
+
 from make_xl_model import get_resulting_workbook_array_for_make
 from make_xl_model import get_array_and_support_variables, fill_array_with_excel_formulas_based_on_is_forecast
 
-abs_filepath = "ref_file.xls"
+abs_filepath = os.path.abspath(os.path.join("examples","ref_file.xls"))
 pivot_col = 2
 sheet = 'model'
 
@@ -27,10 +29,12 @@ update_string = """[['' '' '' 2010.0 2011.0 2012.0 2013.0 2014.0 2015.0 2016.0 2
  ['3. УРАВНЕНИЯ' '' '' '' '' '' '' '' '' '' '' '']
  ['' '' 'x[t] = x_fut[t]' '' '' '' '' '' '' '' '' '']
  ['' '' 'y[t] = y[t-1] * y_rog[t]' '' '' '' '' '' '' '' '' '']]"""
- 
-ar1 = get_resulting_workbook_array_for_make(abs_filepath)
-assert make_string == np.array_str(ar1)
 
-ar, equations_dict = get_array_and_support_variables(abs_filepath, sheet, pivot_col)         
-ar = fill_array_with_excel_formulas_based_on_is_forecast(ar, equations_dict, pivot_col)  
-assert update_string == np.array_str(ar)
+def test_make(): 
+   ar1 = get_resulting_workbook_array_for_make(abs_filepath)
+   assert make_string == np.array_str(ar1)
+
+def test_update():
+   ar, equations_dict = get_array_and_support_variables(abs_filepath, sheet, pivot_col)         
+   ar = fill_array_with_excel_formulas_based_on_is_forecast(ar, equations_dict, pivot_col)  
+   assert update_string == np.array_str(ar)
