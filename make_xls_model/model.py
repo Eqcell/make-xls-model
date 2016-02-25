eb98b687f9e0292a3e6cd74742255087fbe46e57
E Pogrebnyak <e.pogrebnyak@gmail.com>
2016-02-25 19:23:58 +0300

    new interface (#31, #28)

--- make_xls_model/model.py
+++ make_xls_model/model.py
@@ -3,55 +3,53 @@
    Reads inputs from 'data', 'controls', 'equations' and 'names' sheets of <xlfile> and writes 
    resulting spreadsheet to 'model' sheet in <xlfile>. Overwrites 'model' sheet in <xlfile> 
    without warning.  
-
-   Flags and options:   
-   
-       [--from-dataset | -D]        
-
-    --slim or -s produce minimum formatting on 'model' sheet (labels and years only).   
-    -U  Updates Excel formulas on <sheet> only by reading this sheet. Works on output of 
-       'mxm.py -M <xlfile> -f'. Default for <sheet> is 'model'.     
- 
-    [--sheet=<sheet>] 
- 
+       
 Usage:   
     model.py <xlfile> 
     model.py <xlfile> [--from-dataset | -D] [--slim | -s]
-    model.py <xlfile> (--update | -U) [--sheet=<sheet>] 
- 
-   
+    model.py <xlfile> (--update | -U) [--sheet=<name>]   
 """
 
+"""
+   Flags and options:   
+   --from-dataset or -D  derive 'data' and 'controls' sheets content from 'dataset' sheet
+   --slim or -s          produce no extra formatting on 'model' sheet (labels and years only).   
+   --update or -U        update Excel formulas on 'model' sheet or other sheet specified in [--sheet=<name>] 
+"""
 
 from docopt import docopt
 import os
 from make_xl_model import make_xl_model, update_xl_model
 from globals import MODEL_SHEET
 
-def get_abs_filepath(arg):
+def get_filepath(arg):
    """Returns absolute path to <xlfile>"""
    return os.path.abspath(arg["<xlfile>"])
     
 def get_model_sheet(arg):
-   return MODEL_SHEET
+   if arg['--sheet'] is not None:
+       return arg['--sheet']
+   else:
+       return MODEL_SHEET
 
 if __name__ == "__main__":
-   arg = docopt(__doc__)
-    
-   file = get_abs_filepath(arg)
-   sheet = get_model_sheet(arg)    
    
-   # default behaviour is slim formatting
+   arg = docopt(__doc__)
+   
+   file = get_filepath(arg)
+   sheet = get_model_sheet(arg)
+   slim = False
+   use_dataset = False  
+   
+   # slim formatting
    if arg["--slim"] or arg["-s"]:
        slim = True
+       
+   # 'dataset' option 
+   if arg["--from-dataset"] or arg["-D"]:
+       use_dataset = True
+      
+   if arg["-U"] or arg["--update"]:
+      update_xl_model(file, sheet)
    else:
-       slim = False
-   
-   if arg["-U"]:
-      # third column pivot is default output of -M --fancy keys
-      update_xl_model(file, sheet, pivot_col = 2)
-   elif arg["-M"]:
-      make_xl_model(file, sheet, slim)
-   else:
-      raise Exception ("CLI input not specified.")
-   
+      make_xl_model(file, sheet, slim, use_dataset)
