2dfa53ba693a19b59e85b015f3d33e626b879b63
Artyom Veremeenko <arwer13@gmail.com>
2016-03-14 20:21:47 +0300

    Fix indentation

--- make_xls_model/model.py
+++ make_xls_model/model.py
@@ -33,33 +33,33 @@ from make_xl_model import make_xl_model, update_xl_model
 from globals import MODEL_SHEET
 
 def get_filepath(arg):
-   """Returns absolute path to <xlfile>"""
-   return os.path.abspath(arg["<xlfile>"])
+    """Returns absolute path to <xlfile>"""
+    return os.path.abspath(arg["<xlfile>"])
     
 def get_model_sheet(arg):
-   if arg['--sheet'] is not None:
-       return arg['--sheet']
-   else:
-       return MODEL_SHEET
+    if arg['--sheet'] is not None:
+        return arg['--sheet']
+    else:
+        return MODEL_SHEET
 
 if __name__ == "__main__":
    
-   arg = docopt(__doc__)
-   
-   file = get_filepath(arg)
-   sheet = get_model_sheet(arg)
-   slim = False
-   use_dataset = False  
-   
-   # slim formatting
-   if arg["--slim"] or arg["-s"]:
-       slim = True
-       
-   # 'dataset' option 
-   if arg["--use-dataset"] or arg["-D"]:
-       use_dataset = True
-      
-   if arg["-U"] or arg["--update-model"]:
-      update_xl_model(file, sheet)
-   else:
-      make_xl_model(file, sheet, slim, use_dataset)
+    arg = docopt(__doc__)
+
+    file = get_filepath(arg)
+    sheet = get_model_sheet(arg)
+    slim = False
+    use_dataset = False
+
+    # slim formatting
+    if arg["--slim"] or arg["-s"]:
+        slim = True
+
+    # 'dataset' option
+    if arg["--use-dataset"] or arg["-D"]:
+        use_dataset = True
+
+    if arg["-U"] or arg["--update-model"]:
+        update_xl_model(file, sheet)
+    else:
+        make_xl_model(file, sheet, slim, use_dataset)
